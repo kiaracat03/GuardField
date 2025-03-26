b0a5e13ab08f6d32b51f1a0d102fc25a897499d5
kiaracat03 <kiaracat92@gmail.com>
2025-03-26 15:46:19 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/", methods = ["GET", "POST"])
+
+def index():
+    output = None
+    if request.method == "POST":
+        user_input = request.form["user_input"]
+
+        try:
+            # run script.sh with user input and capture output
+            result = subprocess.run(["bash", "script.sh", user_input], capture_output = True, text = True)
+            output = result.stdout 
+            
+            if result.returncode == 0 else f"Error: {result.stderr}"
+
+        except Exception as e:
+            output = f"Execution failed: {str(e)}"
+
+        return render_template("index.html", output = output)
+        
+
+if __name__ == '__main__':
+    app.run(debug = True, port = 5001)
+    # app.run(host = "0.0.0.0", port = 5000, debug = True)
---
+++ script.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "Hello, $1! Your script executed successfully on the Pi."
+
