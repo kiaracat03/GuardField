6a9efd4942e27a0918fda48121f2e67f854788af
kiaracat03 <kiaracat92@gmail.com>
2025-04-29 15:59:12 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,17 @@
-<<<<<<< HEAD
 from flask import Flask, render_template, request
 import subprocess
 
 app = Flask(__name__)
 
 @app.route("/", methods = ["GET", "POST"])
-
 def index():
     output = None
-    scan = None
     if request.method == "POST":
         user_input = request.form["user_input"].strip()
 
         try:
             # run nessus script with user input and capture output
-            scan = subprocess.run(["python3", "nessusV2.py", "quick", user_input])
-            # result = subprocess.run(["python3", "nessusV2.py", "quick", user_input], capture_output = True, text = True)
-            result = subprocess.run(["python3", "nessusV2.py", "last"], capture_output = True, text = True)
-
+            result = subprocess.run(["python3", "nessusV2.py", "quick", user_input], capture_output = True, text = True)
             output = result.stdout if result.returncode == 0 else f"Error: {result.stderr}"
 
         except Exception as e:
@@ -25,34 +19,42 @@ def index():
 
     return render_template("index.html", output = output)
 
-if __name__ == '__main__':
-    app.run(host = "0.0.0.0", port = 5000, debug = True)
-=======
-from flask import Flask, render_template, request
+@app.route("/about", methods = ["GET", "POST"])
+def about_me():
+    return render_template("about_me.html")
 
-app = Flask(__name__)
-
-@app.route("/", methods = ["GET", "POST"])
-
-def index():
+@app.route("/create", methods = ["GET", "POST"])
+def create_scan():
     output = None
     if request.method == "POST":
-        user_input = request.form["user_input"]
+        user_input = request.form["user_input"].strip()
 
         try:
-            # run script.sh with user input and capture output
-            result = subprocess.run(["bash", "script.sh", user_input], capture_output = True, text = True)
-            output = result.stdout 
-            
-            if result.returncode == 0 else f"Error: {result.stderr}"
+            # run nessus script with user input and capture output
+            result = subprocess.run(["python3", "nessusV2.py", "create", user_input], capture_output = True, text = True)
+            output = result.stdout if result.returncode == 0 else f"Error: {result.stderr}"
 
         except Exception as e:
             output = f"Execution failed: {str(e)}"
 
-        return render_template("index.html", output = output)
-        
+    return render_template("create_scan.html", output = output)
+
+@app.route("/vulns", methods = ["GET", "POST"])
+def vulns():
+    output = None
+    if request.method == "POST":
+        # user_input = request.form["user_input"].strip()
+
+        try:
+            # run last vulnerability function
+            result = subprocess.run(["python3", "nessusV2.py", "last"])
+            output = result.stdout if result.returncode == 0 else f"Error: {result.stderr}"
+
+        except Exception as e:
+            output = f"Execution failed: {str(e)}"
+
+    return render_template("vulns.html", output = output)
+
 
 if __name__ == '__main__':
-    app.run(debug = True, port = 5001)
-    # app.run(host = "0.0.0.0", port = 5000, debug = True)
->>>>>>> 64982d46410b735128d7de672f6a62d0f2b36e50
+    app.run(host = "0.0.0.0", port = 5000, debug = True)
